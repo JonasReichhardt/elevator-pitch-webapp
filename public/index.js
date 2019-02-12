6133d1816a41ca94e209afc35c2f91ac8bc5e590
Jonas Reichhardt <IF140023@HTBLAPerg.onmicrosoft.com>
2019-02-12 16:42:46 +0100

    inital

---
+++ public/index.js
@@ -0,0 +1,70 @@
+const socket = io();
+let username;
+let known = false;
+let amount = 0;
+
+var investment = document.getElementById('investment');
+var project = document.getElementById('project');
+var error = document.getElementById("error");
+var login = document.getElementById("login");
+var form = document.getElementById("form");
+var user = document.getElementById('user');
+var money = document.getElementById("amount");
+
+function invest() {
+    const invsetjwer = parseFloat(investment.value);
+    if(invsetjwer < 0){
+        error.innerHTML = "Keine valide Zahl";
+    }else{
+        console.log(invsetjwer,amount);
+        if (amount >= invsetjwer) {
+            console.log(username+' trying to invest '+invsetjwer+' into '+project.value);
+            socket.emit('invest', username, project.value, invsetjwer);
+            error.innerHTML = "";
+        } else {
+            error.innerHTML = "Zu wenig Guthaben";
+        }
+    }
+}
+
+function loginUsr() {
+    username = user.value;
+    if (!known && username != "") {
+        socket.emit('new user', username);
+    }
+    login.style.display = "none";
+    form.style.display = "block";
+}
+
+socket.on('set amount', function (user, AMOUNT) {
+    if (username === user) {
+        amount = AMOUNT;
+        known = true;
+        money.innerHTML = "Guthaben: " + amount;
+    }
+});
+
+socket.on('invested', function (user, AMOUNT) {
+    if (username === user) {
+        amount -= AMOUNT;
+        money.innerHTML = "Guthaben: " + amount;
+    }
+});
+
+socket.on('not a user', function (user){
+    if(user === username){
+        form.style.display = "none";
+        error.innerHTML = "IF-Nummer falsch";
+    }
+});
+
+// add EventListener for Enter event
+window.addEventListener('keydown', function(event){
+    if (event.key === "Enter") {
+        if(login.style.display === "none"){
+            invest();
+        }else{
+            loginUsr();
+        }
+    }
+});
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const http = require('http');
+const sio = require('socket.io');
+const fs = require('fs');
+
+const prjFile = 'projects.json';
+const usrFile = 'users.json';
+const enc = 'utf8';
+
+const app = express();
+const server = http.createServer(app);
+const io = sio(server);
+const port = process.env.PORT || 3000;
+
+var AMOUNT;
+var projects;
+var users;
+
+loadConf();
+setAmount();
+
+//setup server
+server.listen(port, function () {
+    console.log('Server is listening on port:' + port);
+});
+app.use(express.static(__dirname + '/public'));
+
+//Socket.io events
+io.on('connection', function (socket) {
+    socket.on('invest', function (user, project, amount) {
+        if (amount > 0) {
+            if (users.names.indexOf(user) !== -1) {
+                if (projects.names.indexOf(project) !== -1) {
+                    if ((users.value[users.names.indexOf(user)] - amount) >= 0) {
+                        console.log(user + ' hat ' + amount + ' Euro in ' + project + ' investiert');
+                        projects.amount[projects.names.indexOf(project)] += parseFloat(amount);
+                        users.value[users.names.indexOf(user)] -= parseFloat(amount);
+                        socket.emit('invested', user, amount);
+                        console.log(projects.names);
+                        console.log(projects.amount);
+                        console.log('---------------------------------------------------');
+                    }
+                }
+            } else {
+                socket.emit('not a user', user);
+            }
+        }
+    }),
+        socket.on('new user', function (user) {
+            if (users.names.indexOf(user) !== -1) {
+                socket.emit('set amount', user, users.value[users.names.indexOf(user)]);
+            } else {
+                socket.emit('not a user', user);
+            }
+        })
+});
+
+// loading data from JSON files
+function loadConf() {
+    projects = JSON.parse(fs.readFileSync(prjFile), enc);
+    users = JSON.parse(fs.readFileSync(usrFile), enc);
+}
+
+// check command line arguments
+function setAmount() {
+    if (process.argv.length > 2) {
+        AMOUNT = process.argv[2];
+    } else {
+        AMOUNT = 100000;
+    }
+    console.log('Budget pro Benutzer: ' + AMOUNT);
+    for (var i = 0; i < users.names.length; i++) {
+        users.value.push(AMOUNT);
+    }
+}
